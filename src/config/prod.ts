77d7124e4f76b9851e3e21080ccd4074339999a3
lqaxx7799 <lqaxx7799@gmail.com>
2021-12-30 23:21:59 +0700

    update prod path

--- src/config/prod.ts
+++ src/config/prod.ts
@@ -1,7 +1,7 @@
 import ConfigType from './type';
 
 const prod: ConfigType = {
-  BASE_API: 'http://13.250.40.151/api',
+  BASE_API: 'http://54.169.166.121/api',
   GOOGLE_RECAPTCHA_KEY: '6LdjMnocAAAAAPVhUOIPMIM2lNIbulodl66xQvJE',
 };
 
